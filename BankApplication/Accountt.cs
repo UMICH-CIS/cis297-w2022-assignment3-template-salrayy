b6bf91aa4f768c8e03efa0ed55ccdcc30c8208e1
salrayy <79169402+salrayy@users.noreply.github.com>
2022-02-09 20:12:41 -0500

    Add files via upload

---
+++ BankApplication/Accountt.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BankApplication
+{
+    public class Accountt
+    {
+        public int amount
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public void balance()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void initialbalance()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void credit()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void debit()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ BankApplication/checkingaccountt.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BankApplication
+{
+    public class checkingaccountt : Accountt
+    {
+        private int FeeCharged;
+
+        public int FeeAmount
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public void Debit()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Credit()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
