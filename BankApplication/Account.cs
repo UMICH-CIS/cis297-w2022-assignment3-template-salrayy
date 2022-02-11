4795dfc0c04f892abbf9a3b7495f0592ed8aed75
salrayy <79169402+salrayy@users.noreply.github.com>
2022-02-10 22:44:28 -0500

    Add files via upload

---
+++ BankApplication/Account.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Summer Alrayyashi
+/// CIS 297 Winter 2022
+/// This class is the Account class that  retrieves the current balance and if the credit or debit limit has been exceeded.
+/// </summary>
+
+namespace BankApplication
+{
+    public class Account : Program
+    {
+        private decimal balance;
+        public decimal Balance
+        {
+            get
+            { return balance; }
+
+            set
+            { balance = value; }
+        }
+
+        public Account(decimal initialBalance)
+        {
+            if (initialBalance >= 0.0m)
+            {
+                Balance = initialBalance;
+            }
+            else
+            {
+                throw new Exception();
+            }
+
+        }
+
+        public virtual void Credit(decimal Amount)
+        {
+            balance += Amount;
+        }
+
+        public virtual bool Debit(decimal Amount)
+        {
+            if (Amount > balance)
+            {
+                Console.WriteLine("Debit amount exceeded account balance.");
+                return false;
+            }
+            else
+            {
+                balance -= Amount;
+                return true;
+            }
+
+        }
+
+    }
+
+}
---
+++ BankApplication/CheckingAccount.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Summer Alrayyashi
+/// CIS 297 Winter 2022
+/// This class is the Checking Account class that is inherited from the Account class that calculates any necessary fees and charges.
+/// </summary>
+
+namespace BankApplication
+{
+    public class CheckingAccount : Account
+    {
+        //data member
+        private decimal FeeCharged;
+        //constructor
+        public CheckingAccount(decimal Balance, decimal FeeCharged) : base(Balance)
+        {
+            setFeeAmount(FeeCharged);
+        }
+        //member functions
+        void setFeeAmount(decimal FeeCharged)
+        {
+            if (FeeCharged >= 0)
+            {
+                this.FeeCharged = FeeCharged;
+            }
+            else { this.FeeCharged = 0; }
+        }
+        public override void Credit(decimal Balance)
+        {
+            base.Credit(Balance);
+            base.Debit(this.FeeCharged);
+        }
+        public override bool Debit(decimal Balance)
+        {
+            if (base.Debit(Balance))
+            {
+                base.Debit(this.FeeCharged);
+                return true;
+            }
+            return false;
+        }
+
+    }
+
+
+}
